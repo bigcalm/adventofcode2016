0f8f5527a690da41dabef9c4f30572314bf2d292
Iain Cuthbertson <bigcuthy@gmail.com>
2016-12-30 00:38:29 +0000

    Using PHPlot, creates a graph of Day1 input data

--- src/Day1/Puzzle2.php
+++ src/Day1/Puzzle2.php
@@ -2,12 +2,25 @@
 
 namespace Day1;
 
+use PHPlot;
+
 class Puzzle2 extends Puzzle1
 {
     public function processInput(string $input)
     {
         parent::processInput($input);
 
+        $this->setCurrentLocationAsFirstLocationVisitedTwice();
+
+        $this->createGraphFromLocationHistory();
+
+        parent::computeDistance();
+
+        return $this->computedDistance;
+    }
+
+    protected function setCurrentLocationAsFirstLocationVisitedTwice()
+    {
         $locationCounts = [];
 
         foreach ($this->locationHistory as $datum) {
@@ -24,9 +37,35 @@ class Puzzle2 extends Puzzle1
                 break;
             }
         }
+    }
 
-        parent::computeDistance();
+    protected  function createGraphFromLocationHistory()
+    {
+        $data = [];
 
-        return $this->computedDistance;
+        $multiplier = 2;
+
+        foreach ($this->locationHistory as $datum) {
+            $data[] = ['', $datum['x'] * $multiplier, $datum['y'] * $multiplier];
+        }
+
+        $plot = new PHPlot(800 * $multiplier, 600 * $multiplier);
+        $plot->SetImageBorderType('plain');
+
+        $plot->SetPlotType('linepoints');
+        $plot->SetDataType('data-data');
+        $plot->SetDataValues($data);
+
+        $plot->SetPointShapes(['cross']);
+        $plot->SetPointSizes(2 * $multiplier);
+
+        # Main plot title:
+        $plot->SetTitle('Easter Bunny HQ map');
+
+        $plot->SetIsInline(true);
+        $plot->SetFileFormat('png');
+        $plot->SetOutputFile('aoc2016-day1-location-history-graph.png');
+
+        $plot->DrawGraph();
     }
 }
