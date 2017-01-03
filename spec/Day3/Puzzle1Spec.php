4d2834eaba29aa82e1539e5b1f4e24c5a107bdc4
Iain Cuthbertson <iain.cuthbertson@siftware.com>
2017-01-03 19:57:34 +0000

    Day3 Puzzle1 parsing the input

--- spec/Day3/Puzzle1Spec.php
+++ spec/Day3/Puzzle1Spec.php
@@ -8,8 +8,19 @@ use Prophecy\Argument;
 
 class Puzzle1Spec extends ObjectBehavior
 {
+    public $exampleNumbers = "  5 10 25
+";
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Puzzle1::class);
+    }
+
+    public function it_converts_input_sting_into_an_array_and_stores_as_property()
+    {
+        $this->parseString($this->exampleNumbers);
+        $this->getNumberSets()->shouldBeArray();
+        $this->getNumberSets()->shouldHaveCount(1);
+        $this->getNumberSets()[0]->shouldHaveCount(3);
     }
 }
--- src/Day3/Puzzle1.php
+++ src/Day3/Puzzle1.php
@@ -2,6 +2,59 @@
 
 namespace Day3;
 
-class Puzzle1
+use PuzzleInterface;
+
+class Puzzle1 implements PuzzleInterface
 {
+    /** @var array $numberSets */
+    protected $numberSets = [];
+
+    /**
+     * @return array
+     */
+    public function getNumberSets()
+    {
+        return $this->numberSets;
+    }
+
+    /**
+     * @param $array
+     * @return $this
+     */
+    public function setNumberSets($array)
+    {
+        $this->numberSets = $array;
+
+        return $this;
+    }
+
+    public function __construct(array $options = [])
+    {
+        // no options to manage
+    }
+
+    public function processInput(string $input)
+    {
+        $this->parseString($input);
+    }
+
+    public function parseString($inputString)
+    {
+        // remove unwanted CR characters
+        $inputString = str_replace("\r", "\n", $inputString);
+
+        // ensure we only have single newlines for each line
+        $inputString = str_replace("\n\n", "\n", $inputString);
+
+        // split the string into an array
+        $inputArray = explode("\n", trim($inputString));
+
+        // split each row into sets of numbers
+        $this->numberSets = array_map(
+            function($string) {
+                return preg_split('/\s+/', trim($string));
+            },
+            $inputArray
+        );
+    }
 }
