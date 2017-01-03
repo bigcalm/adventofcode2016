586d7cde2a058b1db2a28c8fd6ef755dcf43ba71
Iain Cuthbertson <bigcuthy@gmail.com>
2017-01-03 21:53:42 +0000

    Start of Day4 Puzzle1

---
+++ spec/Day4/Puzzle1Spec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\Day4;
+
+use Day4\Puzzle1;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class Puzzle1Spec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Puzzle1::class);
+    }
+}
---
+++ src/Day4/Puzzle1.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Day4;
+
+use PuzzleInterface;
+
+class Puzzle1 implements PuzzleInterface
+{
+    public function __construct(array $options = [])
+    {
+        // no options to manage
+    }
+
+    public function processInput(string $input)
+    {
+        // TODO: Implement processInput() method.
+    }
+}
