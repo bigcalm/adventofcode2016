28ebe3b265e7190d59064cd97378b18956ccf9e4
Iain Cuthbertson <iain.cuthbertson@siftware.com>
2017-01-04 10:45:20 +0000

    Day4 Puzzle1 parsing the input into a usable array

--- spec/Day4/Puzzle1Spec.php
+++ spec/Day4/Puzzle1Spec.php
@@ -8,8 +8,44 @@ use Prophecy\Argument;
 
 class Puzzle1Spec extends ObjectBehavior
 {
+    public $exampleInput = "aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+";
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Puzzle1::class);
+    }
+
+    public function it_converts_input_sting_into_an_array_and_stores_as_property()
+    {
+        $this->parseString($this->exampleInput);
+        $this->getRooms()->shouldBeArray();
+    }
+
+    public function it_splits_the_raw_room_strings_into_usable_chunks_raw()
+    {
+        $this->parseString($this->exampleInput);
+        $this->getRooms()[0]['raw']->shouldBe('aaaaa-bbb-z-y-x-123[abxyz]');
+    }
+
+    public function it_splits_the_raw_room_strings_into_usable_chunks_checksum()
+    {
+        $this->parseString($this->exampleInput);
+        $this->getRooms()[0]['checksum']->shouldBe('abxyz');
+    }
+
+    public function it_splits_the_raw_room_strings_into_usable_chunks_sector_id()
+    {
+        $this->parseString($this->exampleInput);
+        $this->getRooms()[0]['sector_id']->shouldBe('123');
+    }
+
+    public function it_splits_the_raw_room_strings_into_usable_chunks_encrypted_name()
+    {
+        $this->parseString($this->exampleInput);
+        $this->getRooms()[0]['encrypted_name']->shouldBe('aaaaa-bbb-z-y-x');
     }
 }
--- src/Day4/Puzzle1.php
+++ src/Day4/Puzzle1.php
@@ -6,6 +6,14 @@ use PuzzleInterface;
 
 class Puzzle1 implements PuzzleInterface
 {
+    /** @var array $rooms */
+    protected $rooms = [];
+
+    public function getRooms()
+    {
+        return $this->rooms;
+    }
+
     public function __construct(array $options = [])
     {
         // no options to manage
@@ -13,6 +21,34 @@ class Puzzle1 implements PuzzleInterface
 
     public function processInput(string $input)
     {
-        // TODO: Implement processInput() method.
+        $this->parseString($input);
+
+        // TODO: return a number
+    }
+
+    public function parseString($inputString)
+    {
+        // remove unwanted CR characters
+        $inputString = str_replace("\r", "\n", $inputString);
+
+        // ensure we only have single newlines for each line
+        $inputString = str_replace("\n\n", "\n", $inputString);
+
+        // split the string into an array
+        $inputLines = explode("\n", trim($inputString));
+
+        $this->rooms = array_map(
+            function($string) {
+                preg_match("/(?P<encrypted_name>.+)-(?P<sector_id>\d+)\[(?P<checksum>.+)\]$/", $string, $matches);
+
+                return [
+                    'raw' => $string,
+                    'checksum' => $matches['checksum'],
+                    'sector_id' => $matches['sector_id'],
+                    'encrypted_name' => $matches['encrypted_name'],
+                ];
+            },
+            $inputLines
+        );
     }
 }
