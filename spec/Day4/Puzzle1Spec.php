488ccabdb5a7449c63ce5c010c7e4db21f218944
Iain Cuthbertson <iain.cuthbertson@siftware.com>
2017-01-04 15:28:57 +0000

    Day4 Puzzle1 complete!

--- spec/Day4/Puzzle1Spec.php
+++ spec/Day4/Puzzle1Spec.php
@@ -23,29 +23,69 @@ totally-real-room-200[decoy]
     {
         $this->parseString($this->exampleInput);
         $this->getRooms()->shouldBeArray();
+        $this->getRooms()->shouldHaveKey('aaaaa-bbb-z-y-x-123[abxyz]');
     }
 
     public function it_splits_the_raw_room_strings_into_usable_chunks_raw()
     {
         $this->parseString($this->exampleInput);
-        $this->getRooms()[0]['raw']->shouldBe('aaaaa-bbb-z-y-x-123[abxyz]');
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']->shouldHaveKeyWithValue('raw', 'aaaaa-bbb-z-y-x-123[abxyz]');
     }
 
     public function it_splits_the_raw_room_strings_into_usable_chunks_checksum()
     {
         $this->parseString($this->exampleInput);
-        $this->getRooms()[0]['checksum']->shouldBe('abxyz');
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']->shouldHaveKeyWithValue('checksum', 'abxyz');
     }
 
     public function it_splits_the_raw_room_strings_into_usable_chunks_sector_id()
     {
         $this->parseString($this->exampleInput);
-        $this->getRooms()[0]['sector_id']->shouldBe('123');
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']->shouldHaveKeyWithValue('sector_id', '123');
     }
 
     public function it_splits_the_raw_room_strings_into_usable_chunks_encrypted_name()
     {
         $this->parseString($this->exampleInput);
-        $this->getRooms()[0]['encrypted_name']->shouldBe('aaaaa-bbb-z-y-x');
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']->shouldHaveKeyWithValue('encrypted_name', 'aaaaa-bbb-z-y-x');
+    }
+
+    public function it_calculates_the_frequency_of_each_character_in_the_encrypted_name()
+    {
+        $this->parseString($this->exampleInput);
+        $this->calculateCharacterFrequency($this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']->shouldBeArray();
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']['a']->shouldBe(5);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']['b']->shouldBe(3);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']['x']->shouldBe(1);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']['y']->shouldBe(1);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['character_frequency']['z']->shouldBe(1);
+    }
+
+    public function it_sets_the_top_five_characters()
+    {
+        $this->parseString($this->exampleInput);
+        $this->calculateTopFiveCharacters($this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['top_five_characters']->shouldBe('abxyz');
+    }
+
+    public function it_states_if_a_room_checksum_is_valid()
+    {
+        $this->parseString($this->exampleInput);
+        $this->validateChecksums($this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']);
+        $this->getRooms()['aaaaa-bbb-z-y-x-123[abxyz]']['valid_checksum']->shouldBe(true);
+    }
+
+    public function it_states_if_a_room_checksum_is_invalid()
+    {
+        $this->parseString($this->exampleInput);
+        $this->validateChecksums($this->getRooms()['totally-real-room-200[decoy]']);
+        $this->getRooms()['totally-real-room-200[decoy]']['valid_checksum']->shouldBe(false);
+    }
+
+    public function it_sums_the_sector_ids_of_real_rooms()
+    {
+        $this->parseString($this->exampleInput);
+        $this->sumSectorIdsForRealRooms()->shouldBe(1514);
     }
 }
--- src/Day4/Puzzle1.php
+++ src/Day4/Puzzle1.php
@@ -9,6 +9,9 @@ class Puzzle1 implements PuzzleInterface
     /** @var array $rooms */
     protected $rooms = [];
 
+    /**
+     * @return array
+     */
     public function getRooms()
     {
         return $this->rooms;
@@ -23,7 +26,7 @@ class Puzzle1 implements PuzzleInterface
     {
         $this->parseString($input);
 
-        // TODO: return a number
+        return $this->sumSectorIdsForRealRooms();
     }
 
     public function parseString($inputString)
@@ -37,7 +40,7 @@ class Puzzle1 implements PuzzleInterface
         // split the string into an array
         $inputLines = explode("\n", trim($inputString));
 
-        $this->rooms = array_map(
+        $rooms = array_map(
             function($string) {
                 preg_match("/(?P<encrypted_name>.+)-(?P<sector_id>\d+)\[(?P<checksum>.+)\]$/", $string, $matches);
 
@@ -50,5 +53,91 @@ class Puzzle1 implements PuzzleInterface
             },
             $inputLines
         );
+
+        $this->rooms = array_reduce(
+            $rooms,
+            function($result, $item) {
+                $result[$item['raw']] = $item;
+                return $result;
+            });
+
+        return $this;
+    }
+
+    public function calculateCharacterFrequency($room)
+    {
+        // remove dashes
+        $encryptedName = str_replace('-', '', $room['encrypted_name']);
+
+        $frequency = [];
+
+        // loop over each character and add to frequency array
+        for ($i = 0; $i < strlen($encryptedName); $i++) {
+            $char = substr($encryptedName, $i, 1);
+
+            if (!isset($frequency[$char])) {
+                $frequency[$char] = 0;
+            }
+
+            $frequency[$char]++;
+        }
+
+        $this->rooms[$room['raw']]['character_frequency'] = $frequency;
+
+        return $this;
+    }
+
+    public function calculateTopFiveCharacters($room)
+    {
+        // calculate the character frequency 1st
+        $this->calculateCharacterFrequency($room);
+
+        // work out the 5 characters to be used against the checksum
+        // 1st sort by value
+        // 2nd sort by key
+        $characterFrequency = $this->rooms[$room['raw']]['character_frequency'];
+        array_multisort(
+            array_values($characterFrequency), SORT_DESC,
+            array_keys($characterFrequency), SORT_ASC,
+            $characterFrequency
+        );
+
+        // return the top 5 elements
+        $topFive = array_slice($characterFrequency, 0, 5);
+
+        $this->rooms[$room['raw']]['top_five_characters'] = implode('', array_keys($topFive));
+
+        return $this;
+    }
+
+    public function validateChecksums($room)
+    {
+        // generate real checksum for encrypted room name
+        $this->calculateTopFiveCharacters($room);
+
+        // refetch the room information
+        $room = $this->rooms[$room['raw']];
+
+        // compare against the stored checksum
+        $this->rooms[$room['raw']]['valid_checksum'] = ($room['checksum'] == $room['top_five_characters']);
+
+        return $this;
+    }
+
+    public function sumSectorIdsForRealRooms()
+    {
+        // loop over all of the rooms, validate their checksums and sum the ones that are real
+
+        $sumTotal = 0;
+        foreach ($this->rooms as $room)
+        {
+            $this->validateChecksums($room);
+
+            if ($this->rooms[$room['raw']]['valid_checksum']) {
+                $sumTotal += $room['sector_id'];
+            }
+        }
+
+        return $sumTotal;
     }
 }
