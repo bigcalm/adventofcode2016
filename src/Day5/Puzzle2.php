d103bd4677cb868599b2717d1dadc0c86c0c40d9
Iain Cuthbertson <bigcuthy@gmail.com>
2017-01-05 20:53:23 +0000

    Day5 Puzzle2 passing tests! Just an animation left to do

--- src/Day5/Puzzle2.php
+++ src/Day5/Puzzle2.php
@@ -13,14 +13,15 @@ class Puzzle2 extends Puzzle1 implements PuzzleInterface
         while(true) {
             $md5 = md5($this->input . $this->index);
 
-            $characterPosition = substr($md5, 5, 1);
+            $position = substr($md5, 5, 1);
             $character = substr($md5, 6, 1);
 
             if (substr($md5, 0, 5) === '00000'
-                && preg_match("/^[0-7]$/", $characterPosition)
-                && !isset($this->password[$characterPosition])
+                && preg_match("/^[0-7]$/", $position)
+                && !isset($this->password[$position])
             ) {
-                $this->password[$characterPosition] = $character;
+                $this->password[$position] = $character;
+//                var_dump($this->password);
                 return;
             }
 
@@ -28,4 +29,19 @@ class Puzzle2 extends Puzzle1 implements PuzzleInterface
         }
     }
 
+    public function joinPasswordCharacters(): string
+    {
+        ksort($this->password);
+
+        return parent::joinPasswordCharacters();
+    }
+
+    public function findFullPassword(int $length): string
+    {
+        while(count($this->password) != 8) {
+            $this->findNextCharacterOfPassword();
+        }
+
+        return $this->joinPasswordCharacters();
+    }
 }
