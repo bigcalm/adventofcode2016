50811815781a68df95ca2f5405fadc53baf49808
Iain Cuthbertson <iain.cuthbertson@siftware.com>
2017-01-05 17:30:35 +0000

    Day5 Puzzle1 complete!

--- spec/Day5/Puzzle1Spec.php
+++ spec/Day5/Puzzle1Spec.php
@@ -8,10 +8,57 @@ use Prophecy\Argument;
 
 class Puzzle1Spec extends ObjectBehavior
 {
-    public $exampleInput = 'abc';
+    public $exampleInput = 'abc
+    ';
 
     function it_is_initializable()
     {
         $this->shouldHaveType(Puzzle1::class);
+    }
+
+    public function it_parses_the_input_into_a_property()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->input->shouldBe('abc');
+    }
+
+    public function it_finds_the_1st_password_character()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->findNextCharacterOfPassword();
+        $this->password[0]->shouldBe('1');
+        $this->index->shouldBe(3231929);
+    }
+
+    public function it_finds_the_2nd_password_character()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->index = 3231929;
+        $this->findNextCharacterOfPassword();
+        $this->password[0]->shouldBe('8');
+        $this->index->shouldBe(5017308);
+    }
+
+    public function it_finds_the_3rd_password_character()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->index = 5017308;
+        $this->findNextCharacterOfPassword();
+        $this->password[0]->shouldBe('f');
+    }
+
+    public function it_returns_a_multi_character_password()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->findNextCharacterOfPassword();
+        $this->findNextCharacterOfPassword();
+        $this->findNextCharacterOfPassword();
+        $this->joinPasswordCharacters()->shouldBe('18f');
+    }
+
+    public function it_finds_8_password_characters()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->findFullPassword(8)->shouldBe('18f47a30');
     }
 }
--- src/Day5/Puzzle1.php
+++ src/Day5/Puzzle1.php
@@ -6,6 +6,13 @@ use PuzzleInterface;
 
 class Puzzle1 implements PuzzleInterface
 {
+    /** @var string $input */
+    public $input = '';
+
+    public $index = 0;
+
+    public $password = [];
+
     public function __construct(array $options = [])
     {
         // no options to manage
@@ -13,6 +20,48 @@ class Puzzle1 implements PuzzleInterface
 
     public function processInput(string $input)
     {
+        $this->parseInput($input);
+        return $this->findFullPassword(8);
+    }
 
+    /**
+     * @param string $input
+     * @return Puzzle1
+     */
+    public function parseInput(string $input): Puzzle1
+    {
+        $this->input = trim($input);
+
+        return $this;
+    }
+
+    public function findNextCharacterOfPassword()
+    {
+        $this->index++;
+
+        while(true) {
+            $md5 = md5($this->input . $this->index);
+
+            if (substr($md5, 0, 5) === '00000') {
+                $this->password[] = substr($md5, 5, 1);
+                return;
+            }
+
+            $this->index++;
+        }
+    }
+
+    public function joinPasswordCharacters(): string
+    {
+        return implode('', $this->password);
+    }
+
+    public function findFullPassword(int $length): string
+    {
+        for ($i = 0; $i < $length; $i++) {
+            $this->findNextCharacterOfPassword();
+        }
+
+        return $this->joinPasswordCharacters();
     }
 }
