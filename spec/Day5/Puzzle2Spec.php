890f6acecb54a9da21467c34ea364dc4072c431c
Iain Cuthbertson <iain.cuthbertson@siftware.com>
2017-01-05 19:39:06 +0000

    Working on Day5 Puzzle2. Not quite there yet

--- spec/Day5/Puzzle2Spec.php
+++ spec/Day5/Puzzle2Spec.php
@@ -8,8 +8,32 @@ use Prophecy\Argument;
 
 class Puzzle2Spec extends ObjectBehavior
 {
+    public $exampleInput = 'abc';
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Puzzle2::class);
     }
+
+    public function it_stores_the_password_character_in_position_1_based_upon_hash()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->findNextCharacterOfPassword();
+        $this->password[1]->shouldBe('5');
+    }
+
+    public function it_stores_the_password_character_in_position_4_based_upon_hash()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->index = 3231929;
+        $this->findNextCharacterOfPassword();
+        $this->password[4]->shouldBe('e');
+    }
+
+    public function it_finds_8_password_characters()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->findFullPassword(8)->shouldBe('05ace8e3');
+    }
+
 }
--- src/Day5/Puzzle2.php
+++ src/Day5/Puzzle2.php
@@ -6,4 +6,26 @@ use PuzzleInterface;
 
 class Puzzle2 extends Puzzle1 implements PuzzleInterface
 {
+    public function findNextCharacterOfPassword()
+    {
+        $this->index++;
+
+        while(true) {
+            $md5 = md5($this->input . $this->index);
+
+            $characterPosition = substr($md5, 5, 1);
+            $character = substr($md5, 6, 1);
+
+            if (substr($md5, 0, 5) === '00000'
+                && preg_match("/^[0-7]$/", $characterPosition)
+                && !isset($this->password[$characterPosition])
+            ) {
+                $this->password[$characterPosition] = $character;
+                return;
+            }
+
+            $this->index++;
+        }
+    }
+
 }
