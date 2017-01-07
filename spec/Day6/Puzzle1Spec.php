fb42505a4c284023f2f7926ffeaa08b5b0bcb4df
Iain Cuthbertson <bigcuthy@gmail.com>
2017-01-07 17:11:11 +0000

    Day6 Puzzle1 complete!

--- spec/Day6/Puzzle1Spec.php
+++ spec/Day6/Puzzle1Spec.php
@@ -30,4 +30,56 @@ enarar';
     {
         $this->shouldHaveType(Puzzle1::class);
     }
+
+    public function it_converts_input_into_stored_array()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->inputLines->shouldBeArray();
+    }
+
+    public function it_sets_target_length_as_property()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->setTargetStringLength();
+        $this->targetStringLength->shouldBe(6);
+    }
+
+    public function it_creates_an_array_to_hold_letter_counts()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->setTargetStringLength();
+        $this->addLettersFromInput($this->inputLines[0]);
+        $this->letters->shouldHaveCount(6);
+        for ($i = 0; $i < 6; $i++) {
+            $this->letters[$i]->shouldBeArray();
+        }
+    }
+
+    public function it_adds_letter_counts_to_the_letters_parameter()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->setTargetStringLength();
+        $this->addLettersFromInput($this->inputLines[0]);
+        $this->letters[0]->shouldhaveCount(1);
+        $this->letters[0]['e']->shouldBe(1);
+        $this->letters[1]['e']->shouldBe(1);
+        $this->letters[2]['d']->shouldBe(1);
+        $this->letters[3]['a']->shouldBe(1);
+        $this->letters[4]['d']->shouldBe(1);
+        $this->letters[5]['n']->shouldBe(1);
+    }
+
+    public function it_loops_over_full_input_and_increments_letter_counts()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->decodeInput();
+        $this->letters[0]['e']->shouldBe(3);
+    }
+
+    public function it_decodes_input_to_reveal_answer()
+    {
+        $this->parseInput($this->exampleInput);
+        $this->decodeInput();
+        $this->message->shouldBe('easter');
+    }
 }
--- src/Day6/Puzzle1.php
+++ src/Day6/Puzzle1.php
@@ -6,6 +6,18 @@ use PuzzleInterface;
 
 class Puzzle1 implements PuzzleInterface
 {
+    /** @var array $input */
+    public $inputLines = [];
+
+    /** @var int $targetStringLength */
+    public $targetStringLength = 0;
+
+    /** @var array $letters */
+    public $letters = [];
+
+    /** @var string $message */
+    public $message = '';
+
     public function __construct(array $options = [])
     {
         // no configuration options
@@ -13,6 +25,63 @@ class Puzzle1 implements PuzzleInterface
 
     public function processInput(string $input)
     {
-        // TODO: Implement processInput() method.
+        $this->parseInput($input);
+        $this->decodeInput();
+
+        return $this->message;
+    }
+
+    public function parseInput($inputString)
+    {
+        // remove unwanted CR characters
+        $inputString = str_replace("\r", "\n", $inputString);
+
+        // ensure we only have single newlines for each line
+        $inputString = str_replace("\n\n", "\n", $inputString);
+
+        // split the string into an array
+        $this->inputLines = explode("\n", trim($inputString));
+
+        $stringLength = strlen($this->inputLines[0]);
+    }
+
+    public function setTargetStringLength()
+    {
+        $this->targetStringLength = strlen($this->inputLines[0]);
+
+        return $this;
+    }
+
+    public function addLettersFromInput($string)
+    {
+        for ($i = 0; $i < $this->targetStringLength; $i++) {
+            if (!isset($this->letters[$i])) {
+                $this->letters[$i] = [];
+            }
+
+            $letter = substr($string, $i, 1);
+            if (!isset($this->letters[$i][$letter])) {
+                $this->letters[$i][$letter] = 0;
+            }
+
+            $this->letters[$i][$letter]++;
+        }
+    }
+
+    public function decodeInput()
+    {
+        $this->setTargetStringLength();
+
+        // count each letter frequency per column
+        foreach ($this->inputLines as $inputLine) {
+            $this->addLettersFromInput($inputLine);
+        }
+
+        // add most frequent letters to output message
+        foreach ($this->letters as $index => $letterCounts) {
+            arsort($this->letters[$index]);
+
+            $this->message .= array_keys($this->letters[$index])[0];
+        }
     }
 }
